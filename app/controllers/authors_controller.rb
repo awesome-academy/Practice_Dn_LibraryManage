f8ad87de39b120fd0a82059058087ea6d3bb9a67
BuBuHuy <luuvanhuy2012@gmail.com>
2020-07-24 10:09:14 +0700

    List-ShowDetails-Search Authors Done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,33 @@
 class AuthorsController < ApplicationController
-  def new; end
+  before_action :find_author, only: [:show ]
+
+  def new
+    @author = Author.new
+  end
+
+  def index
+    @search = !params[:keyword].nil? ? Author.where('lower(name) LIKE ?', "%#{params[:keyword]}%") : Author.all
+    @pagy, @authors = pagy(@search.order('created_at DESC'), items: 9)
+  end
+
+  def show
+    @q = Author.search(params[:q])
+    @search = @q.result(distinct: true)
+    @books = @author.books
+    @authors = Author.all
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :email, :info)
+  end
+
+  def find_author
+    @author = Author.find_by(id: params[:id])
+    return if @author
+
+    flash[:danger] = I18n.t("error")
+    redirect_to authors_path
+  end
 end
