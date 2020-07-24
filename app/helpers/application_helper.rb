963596fe5ce034820e9c8947832f1b59ef806600
BuBuHuy <luuvanhuy2012@gmail.com>
2020-07-24 10:09:14 +0700

    List-ShowDetails-Search Books Done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def cart
+    @cart = Cart.where(user_id: current_user.id).last if current_user 
+    if @cart 
+      return @cart
+    else 
+      return nil
+    end
+end
+  def requests
+    @cart = cart
+    if @cart.nil? || @cart.verify != 3
+      nil
+    else
+      @requests = Request.where(cart_id: @cart.id).order('created_at DESC')
+      @requests
+    end
+  end
+
+  # kiem tra xem item co trong gio chua
+  def check_item_cart(book_id)
+    return if requests.nil?
+
+    check = (book_ids requests).include? book_id
+ end
 end
